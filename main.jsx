9078d071c3b1ae1cb674be70953c08a2bc95cbcf
RJOSmith <bedfordhatter@gmail.com>
2025-07-15 19:32:25 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GCSEMockSite from './GcseQuickMocksSite';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GCSEMockSite />
-  </React.StrictMode>
-);
